fb8d9028af80483f2b421b6ff740ca271e83b177
Nguyen Anh <nngguyen.anh@gmail.com>
2023-03-21 18:51:44 +0700

    LAB - 02 | solve problem 6

--- Lab2_MapReduce-programing/src/problem06/AverageSalary.java
+++ Lab2_MapReduce-programing/src/problem06/AverageSalary.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable;
@@ -10,50 +11,59 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AverageSalary {
-   public static class avgMapper extends Mapper<Object, Text, Text, FloatWritable> {
-      private Text dept_id = new Text();
-      private FloatWritable salary = new FloatWritable();
 
-      public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+  public static class AverageSalaryMapper
+    extends Mapper<Object, Text, Text, FloatWritable> {
 
-         String values[] = value.toString().split("\t");
-         dept_id.set(values[0]);
-         salary.set(Float.parseFloat(values[2]));
-         context.write(dept_id, salary);
+    public void map(Object key, Text value, Context context)
+      throws IOException, InterruptedException {
+      // split the line with format: dept_id, emp_id (guess), salary
+      StringTokenizer itr = new StringTokenizer(value.toString());
+      String deptId = itr.nextToken();
+      String empId = itr.nextToken();
+      String salary = itr.nextToken();
+
+      // write output as format <deptId, salary>
+      context.write(
+        new Text(deptId),
+        new FloatWritable(Float.parseFloat(salary))
+      );
+    }
+  }
+
+  public static class AverageSalaryReducer
+    extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+
+    public void reduce(
+      Text key,
+      Iterable<FloatWritable> values,
+      Context context
+    ) throws IOException, InterruptedException {
+      float sum = 0;
+      float count = 0;
+
+      // get total salary of the department and count number of employees as well
+      for (FloatWritable val : values) {
+        sum += val.get();
+        count += 1;
       }
-   }
 
-   public static class avgReducer extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-      private FloatWritable result = new FloatWritable();
+      // write the average salary of the department = sum / count
+      context.write(key, new FloatWritable(sum / count));
+    }
+  }
 
-      public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException,
-
-            InterruptedException {
-
-         float sum = 0;
-         float count = 0;
-         for (FloatWritable val : values) {
-            sum += val.get();
-            count++;
-         }
-         result.set(sum / count);
-         context.write(key, result);
-      }
-   }
-
-   public static void main(String[] args) throws Exception {
-      Configuration conf = new Configuration();
-      Job job = new Job(conf, "averagesal");
-      job.setJarByClass(AverageSalary.class);
-      job.setMapperClass(avgMapper.class);
-      job.setCombinerClass(avgReducer.class);
-      job.setReducerClass(avgReducer.class);
-      job.setOutputKeyClass(Text.class);
-      job.setOutputValueClass(FloatWritable.class);
-      Path p = new Path(args[0]);
-      Path p1 = new Path(args[1]);
-      FileInputFormat.addInputPath(job, p);
-      FileOutputFormat.setOutputPath(job, p1);
-      job.waitForCompletion(true);
-   }
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = new Job(conf, "average salary");
+    job.setJarByClass(AverageSalary.class);
+    job.setMapperClass(AverageSalaryMapper.class);
+    job.setCombinerClass(AverageSalaryReducer.class);
+    job.setReducerClass(AverageSalaryReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(FloatWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.waitForCompletion(true);
+  }
 }
